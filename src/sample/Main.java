ef17b775abad400bf87b35b3666967d7dd1779d1
ADAsss98 <224806@edu.p.lodz.pl>
2021-06-18 22:27:36 +0200

    final graphic changes and add icon

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -17,6 +18,7 @@ public class Main extends Application {
         scene.getStylesheets().add("sample/style/stylesheet.css");
         primaryStage.setScene(scene);
         primaryStage.show();
+        primaryStage.getIcons().add(new Image("sample/assets/logo.png"));
     }
 
 
